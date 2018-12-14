d1a83971af49020efe1442ebcebdd47120adb399
Mohamed_Hassanin <mohamed.pop871@yahoo.com>
2018-12-14 13:05:26 +0200

    update test3
    Don't consider the ground a node.

--- test3/test3/test3.cpp
+++ test3/test3/test3.cpp
@@ -13,22 +13,24 @@ int main()
 	float secondNodeArr[3] = { 0 , 2 , 0 };
 	float valueArr[3]{ 5 , 1, 6 };
 
-	int n = 3, m = 2;
+	int n = 2, m = 2;
 	MatrixXd A = MatrixXd::Zero(n, m); //initialize all coefficient to 0
 	MatrixXd B = MatrixXd::Zero(m, n);
 	
 	int lastIndex =0;
+	int numOfComponents = 3;
+
 	for (int i = 0; i < m; i++) {//coulmns //FIFO (first in first out) method in dealing with Vs
-		for (int k = lastIndex; k < n; k++) { //reads the next voltage source
+		for (int k = lastIndex; k < numOfComponents; k++) { //reads the next voltage source
 			if (typeArr[k] == 'V') {
 				lastIndex = k + 1;
 				if(firstNodeArr[k] != 0){
-					A(firstNodeArr[k], i) = 1;
-					B(i, firstNodeArr[k]) = 1;
+					A(firstNodeArr[k] - 1, i) = 1;
+					B(i, firstNodeArr[k] - 1) = 1;		//-1 because it's index now
 				}
 				if (secondNodeArr[k] != 0) {
-					A(secondNodeArr[k], i) = -1;
-					B(i, secondNodeArr[k]) = -1;
+					A(secondNodeArr[k] - 1, i) = -1;
+					B(i, secondNodeArr[k] - 1) = -1;
 				}
 				break;
 			}
